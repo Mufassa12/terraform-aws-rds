ce7b67b2b7fcdc3ef3e773b4ef8160fd6256f5c5
Shaun S <shaun.stuart@gmail.com>
2022-10-09 22:31:28 +1300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 output "rds_hostname" {
   description = "RDS instance hostname"
-  value       = aws_db_instance.educationss.address
+  value       = aws_db_instance.edulearning.address
 }
 
 output "rds_port" {
   description = "RDS instance port"
-  value       = aws_db_instance.educationss.port
+  value       = aws_db_instance.edulearning.port
 }
 
 output "rds_username" {
   description = "RDS instance root username"
-  value       = aws_db_instance.educationss.username
+  value       = aws_db_instance.edulearning.username
 }
 

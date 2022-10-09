445ff2db85990ddfdafc08d2d3370219f40e369c
Shaun S <shaun.stuart@gmail.com>
2022-10-09 20:35:01 +1300

    Update main.tf

--- main.tf
+++ main.tf
@@ -73,7 +73,7 @@ resource "aws_db_instance" "educationshaun" {
   username               = var.db_username
   password               = var.db_password
   db_subnet_group_name   = aws_db_subnet_group.educationshaun.name
-  vpc_security_group_ids = [aws_security_group.rds.id]
+  vpc_security_group_ids = [aws_security_group.rdsshaun.id]
   parameter_group_name   = aws_db_parameter_group.educationshaun.name
   publicly_accessible    = true
   skip_final_snapshot    = true
